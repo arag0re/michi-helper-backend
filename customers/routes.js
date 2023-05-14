9e1d7a953b6f88e9939bff5f1c3824116877b1c7
Anton Stadie <anton.stadie@icontec.de>
2023-05-14 14:01:04 +0200

    updated delete route

--- customers/routes.js
+++ customers/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const AppSchema = require('../models/app')
 const CustomerSchema = require('../models/customer')
+const App = mongoose.model('App', AppSchema)
 const Customer = mongoose.model('Customer', CustomerSchema)
 
 // Create controller for creation, updating of customers
@@ -62,8 +64,15 @@ customerController.deleteCustomer = async (req, res) => {
 
       const customer = await Customer.findOneAndDelete({ name })
       if (!customer) {
-         return res.status(403).send('App does not exist')
+         return res.status(403).send('Customer does not exist')
       }
+
+      customer.apps.forEach(async (app) => {
+         const _app = await App.findByIdAndDelete(app)
+         if (!_app) {
+            return res.status(403).send('App does not exist')
+         }
+      })
 
       res.status(200).send()
    } catch (err) {
